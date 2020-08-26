353f7d7b4b86f575bef77a1863c6d12e87536224
Antonio Decanini <antonio.decanini@hotmail.com>
2020-08-26 11:49:07 -0300

    Add logout function

--- mobile/src/routers/DrawerNavigator.js
+++ mobile/src/routers/DrawerNavigator.js
@@ -1,18 +1,67 @@
 import React from "react";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import {
+  createDrawerNavigator,
+  DrawerItem,
+  DrawerContentScrollView,
+} from "@react-navigation/drawer";
+import { useNavigation } from "@react-navigation/native";
 
 import Logout from "../components/logout";
 import MyAccount from "../pages/myAccount";
 import Locals from "../pages/locals";
 
+import { logout } from "../services/auth";
+
 const { Navigator, Screen } = createDrawerNavigator();
 
 export default function Drawer() {
+  const navigation = useNavigation();
+
+  function handleLogout() {
+    logout();
+
+    navigation.navigate("SignIn");
+  }
+
+  function CustomDrawerContent() {
+    return (
+      <DrawerContentScrollView>
+        <DrawerItem label="Sair" onPress={() => handleLogout()} />
+      </DrawerContentScrollView>
+    );
+  }
+
   return (
-    <Navigator initialRouteName="Locals">
-      <Screen name="Locals" component={Locals} />
-      <Screen name="MyAccount" component={MyAccount} />
-      <Screen name="Logout" component={Logout} />
+    <Navigator
+      drawerContent={() => <CustomDrawerContent />}
+      initialRouteName="Locals"
+      drawerStyle={{
+        backgroundColor: "#fff5fa",
+        width: "65%",
+        borderRadius: 5,
+      }}
+    >
+      <Screen
+        name="Locals"
+        component={Locals}
+        options={{
+          drawerLabel: "Locais Disponiveis",
+        }}
+      />
+      <Screen
+        name="MyAccount"
+        component={MyAccount}
+        options={{
+          drawerLabel: "Minha conta",
+        }}
+      />
+      <Screen
+        name="Logout"
+        component={Logout}
+        options={{
+          drawerLabel: "Sair",
+        }}
+      />
     </Navigator>
   );
 }
--- mobile/src/services/auth.js
+++ mobile/src/services/auth.js
@@ -1,4 +1,4 @@
-import { AsyncStorage } from 'react-native'
+import { AsyncStorage } from "react-native";
 
 export const TOKEN_KEY = "@doelocais-Token";
 
@@ -6,10 +6,10 @@ export const isAuthenticated = () => AsyncStorage.getItem(TOKEN_KEY) !== null;
 
 export const getToken = () => AsyncStorage.getItem(TOKEN_KEY);
 
-export const login = token => {
-    AsyncStorage.setItem(TOKEN_KEY, token);
+export const login = (token) => {
+  AsyncStorage.setItem(TOKEN_KEY, token);
 };
 
 export const logout = () => {
-    AsyncStorage.removeItem(TOKEN_KEY);
+  AsyncStorage.removeItem(TOKEN_KEY);
 };
